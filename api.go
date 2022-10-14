d8c4eeb7c48142aea2819a17d3e16a7c5f76eda0
sssrot <sunss@tutanota.com>
2022-10-14 16:15:21 +0800

    fix warn msg

--- api.go
+++ api.go
@@ -12,6 +12,8 @@ var instance *Instance
 
 var works = false
 
+const warnMsg = "WARN (rust2go): U128BitsToFix return value may be wrong, please do not use this library!"
+
 /***
 In practice, U128BitsToFix got wrong value some time.
 IsWork() indicate whether it works for known test case.
@@ -28,25 +30,28 @@ func init() {
 	}
 	instance = inst
 
-	testCase := struct{ input, output string }{"1341723281558402372940796526592", "72734964837"}
+	testCase := struct{ input, output string }{"1341723281558402372940796526592", "72734964837"} //known test case that fail at some machine
 	testBig, _ := big.NewInt(0).SetString(testCase.input, 10)
-	ret, err := U128BitsToFix(context.Background(), testBig)
+	ret, err := inst.U128BitsToFix(context.Background(), testBig)
 	if err != nil {
 		panic(err)
 	}
 	works = ret.String() == testCase.output
+	if !works {
+		log.Println(warnMsg)
+	}
 }
 
 func F64ToFixBits(ctx context.Context, f float64) (*big.Int, error) {
 	if !works {
-		log.Println("WARN (rust2go): F64ToFixBits return value may be wrong, please do not use this library!")
+		log.Println(warnMsg)
 	}
 	return instance.F64ToFixBits(ctx, f)
 }
 
 func U128BitsToFix(ctx context.Context, b *big.Int) (decimal.Decimal, error) {
 	if !works {
-		log.Println("WARN (rust2go): U128BitsToFix return value may be wrong, please do not use this library!")
+		log.Println(warnMsg)
 	}
 	return instance.U128BitsToFix(ctx, b)
 }
